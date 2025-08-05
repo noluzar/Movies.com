f95958fb911e2b62cbffc7ba1ea0d62b77ed9772
Noluthando Ndlovu <noluthandosamantha.ns@gmail.com>
2025-08-05 13:14:50 +0200

    app centered

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import MoviesSliced from "../components/MoviesSliced";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="w-[70%]">
       <Navbar />
       <Home />
       <div className="absolute top-20 left-1/2 transform -translate-x-1/2 w-full">
